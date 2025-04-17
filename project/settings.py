c56a75e7bf07fce61295583c5065d7af7be0b0fc
Ahmed Hashim <ahmedha4im7@gmail.com>
2025-04-17 09:33:26 +0200

    Update database settings to use PostgreSQL and add requirements for project dependencies

--- project/settings.py
+++ project/settings.py
@@ -95,22 +95,22 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'trader',
-#         'USER': 'ahmed',
-#         'PASSWORD': 'MyS3cur3Pass',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'trader',
+        'USER': 'ahmed',
+        'PASSWORD': 'MyS3cur3Pass',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 
