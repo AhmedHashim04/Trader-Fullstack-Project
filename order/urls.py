b2ae72cc7db6d19d0f5cc455adc196b2462fec65
Ahmed Hashim <ahmedha4im7@gmail.com>
2025-04-30 17:33:03 +0300

    Refactor order preview and confirmation flow; enhance order details presentation, improve form handling, and update URL routing for better clarity and user experience.

--- order/urls.py
+++ order/urls.py
@@ -1,14 +1,22 @@
 from django.urls import path
-from .views import create_order, OrderListView, OrderDetailView, CheckoutView, clear_order_history , confirm_order
+from .views import (
+    create_order,
+    confirm_order,
+    OrderListView,
+    OrderDetailView,
+    CheckoutView,
+    PreviewView,
+    clear_order_history,
+)
 
 app_name = 'order'
 
 urlpatterns = [
-    path('orders/', OrderListView.as_view(), name='order_list'),
-    path('order/<str:id>/', OrderDetailView.as_view(), name='order_detail'),
+    path('orders/', OrderListView.as_view(), name='order_list'), 
+    path('order/<str:id>/', OrderDetailView.as_view(), name='order_detail'),  
     path('checkout/<str:id>/', CheckoutView.as_view(), name='checkout'),
-    path('create-order/', create_order, name='create_order'),
-    path('clear-order-history/', clear_order_history, name='clear_order_history'),
-    path('confirm/<str:confirmation_key>/', confirm_order, name='confirm_order'),
-
+    path('preview/', PreviewView.as_view(), name='preview_order'), 
+    path('create-order/', create_order, name='create_order'), 
+    path('confirm/<str:confirmation_key>/', confirm_order, name='confirm_order'),  
+    path('clear-order-history/', clear_order_history, name='clear_order_history'),  
 ]
