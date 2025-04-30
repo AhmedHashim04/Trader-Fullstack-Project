55166472715ec3ac06573b99abc449f3c97e4f1c
Ahmed Hashim <ahmedha4im7@gmail.com>
2025-04-30 12:33:21 +0300

    Refactor cart functionality and templates for improved user experience; enhance cart item handling, update URLs, and add order confirmation page.

--- order/urls.py
+++ order/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import create_order, OrderListView, OrderDetailView, clear_order_history , confirm_order
+from .views import create_order, OrderListView, OrderDetailView, CheckoutView, clear_order_history , confirm_order
 
 app_name = 'order'
 
 urlpatterns = [
     path('orders/', OrderListView.as_view(), name='order_list'),
     path('order/<str:id>/', OrderDetailView.as_view(), name='order_detail'),
+    path('checkout/<str:id>/', CheckoutView.as_view(), name='checkout'),
     path('create-order/', create_order, name='create_order'),
     path('clear-order-history/', clear_order_history, name='clear_order_history'),
     path('confirm/<str:confirmation_key>/', confirm_order, name='confirm_order'),
